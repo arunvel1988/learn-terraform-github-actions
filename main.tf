08a2b6e8a7747274600ab38c142558151fcf1883
arunvel1988 <aryan.arunachalam@gmail.com>
2023-02-08 15:27:54 +0530

    terraform repo aws changes

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "arunvel"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
 resource "aws_instance" "test" {
   ami           = "ami-06984ea821ac0a879" # Ubuntu 18.04 LTS
   instance_type = "t2.micro"
